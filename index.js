ab290d725068ce6dd323eb551e6efadf674ff173
Dhaval Mehta <dhruvin.83@gmail.com>
2026-08-20 14:49:14 +0530

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('{"message": "Hello"}');
 });
+const port = process.env.PORT || 4000 
 // starts a simple http server locally on port 3000
-server.listen(10000, '0.0.0.0', () => {
+server.listen(port, '0.0.0.0', () => {
   console.log('Listening on 127.0.0.1:10000');
 });
